f273564c4bc7aefcd6f2556a4a8c1fc2dfbbb284
z7pz <sdfygamer@gmail.com>
2022-06-25 08:29:28 +0300

    feat: add search input and settings icon

--- src/components/App/ChannelBar/ChannelBar.tsx
+++ src/components/App/ChannelBar/ChannelBar.tsx
@@ -1,13 +1,26 @@
-import { h } from 'preact';
-import styles from '../../../styles/App/ChannelBar.module.css'
-export const ChannelBar = ({ name }: {name: string}) => {
-    return (
-        <div className={styles.channelBar}>
-            <div className={styles.channelBarContainer}>
-                <div className={styles.hash}>#</div>
-                <div className={styles.divider} />
-                <div className={styles.name}>{name}</div>
-            </div>
+import { h } from "preact";
+import styles from "../../../styles/App/ChannelBar.module.css";
+import { Search, Settings } from "tabler-icons-react";
+import { Input } from "@mantine/core";
+
+export const ChannelBar = ({ name }: { name: string }) => {
+  return (
+    <div className={styles.channelBar}>
+      <div className={styles.channelBarContainer}>
+        <div className={styles.hash}>#</div>
+        <div className={styles.divider} />
+        <div className={styles.name}>{name}</div>
+        <div className={styles.settings}>
+          <Settings size={32} color={"rgba(255, 255, 255, 0.5)"} />
         </div>
-    );
+        <Input
+          className={styles.searchInput}
+          size="lg"
+          variant="unstyled"
+          icon={<Search size={22} />}
+          placeholder="Search"
+        />
+      </div>
+    </div>
+  );
 };
