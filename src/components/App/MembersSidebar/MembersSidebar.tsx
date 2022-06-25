482ac3d3d06d849dfe3d0592e32f82511913e20f
z7pz <sdfygamer@gmail.com>
2022-06-25 17:10:34 +0300

    feat: Members side bar compleated

--- src/components/App/MembersSidebar/MembersSidebar.tsx
+++ src/components/App/MembersSidebar/MembersSidebar.tsx
@@ -1,9 +1,29 @@
 import { h } from "preact";
+import { UserIcon } from "../../utils";
 import styles from "../../../styles/App/MembersSidebar.module.css";
-export const MembersSidebar = () => {
+export const MembersSidebar = ({ roles }: { roles: any[] }) => {
   return (
     <div className={styles.membersSidebar}>
-      <div class={styles.membersSidebarContainer}></div>
+      <div class={styles.membersSidebarContainer}>
+        {roles.map((role) => (
+          <div className={styles.roleContainer}>
+            <div className={styles.roleName}>{role.name}</div>
+            <div className={styles.roleMembers}>
+              {role.members.map((member: any) => (
+                <div className={styles.roleMember}>
+                  <UserIcon
+                    name={member.name}
+                    avatar={member.avatar}
+                    status="online"
+                  />
+                  <div className={styles.roleMemberName}>{member.name}</div>
+                  <div className={styles.roleMemberStatus}>online</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,4 +1,5 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import {
   ChatArea,
   MainSidebar,
@@ -8,13 +9,35 @@ import {
 import { ChannelBar } from "../../components/App/ChannelBar";
 import styles from "../../styles/App/index.module.css";
 export const AppPage = () => {
+  const [roles, setRoles] = useState([
+    {
+      name: "Founder",
+      color: "red",
+      members: [
+        { name: "NeverMind", avatar: "" },
+        { name: "NeverMind", avatar: "" },
+        { name: "NeverMind", avatar: "" },
+      ],
+    },
+
+    {
+      name: "Admins",
+      color: "blue",
+      members: [
+        { name: "NeverMind", avatar: "" },
+        { name: "NeverMind", avatar: "" },
+        { name: "NeverMind", avatar: "" },
+      ],
+    },
+  ]);
+
   return (
     <div className={styles.app}>
       <ServersSidebar />
       <MainSidebar />
       <ChannelBar name="welcome" />
       <ChatArea />
-      <MembersSidebar />
+      <MembersSidebar roles={roles} />
     </div>
   );
 };
