32cbc8167fcbac583da4f694891748e7d7ca612e
z7pz <sdfygamer@gmail.com>
2022-07-21 04:06:36 +0300

    feat: implement the ui

--- src/containers/pages/Application.tsx
+++ src/containers/pages/Application.tsx
@@ -1,5 +1,19 @@
 import { h } from "preact";
 
+import styles from "./styles/Application.module.scss";
+
 export function Application() {
-  return <div>Application page</div>;
+  return (
+    <div class={styles.container}>
+      <div class={styles.navbar}>
+        <div class={styles.servers_navbar}></div>
+        <div class={styles.application_navbar}></div>
+        <div class={styles.members_navbar}></div>
+      </div>
+      <div class={styles.servers_sidebar}></div>
+      <div class={styles.server_sidebar}></div>
+      <div class={styles.application}></div>
+      <div class={styles.members_sidebar}></div>
+    </div>
+  );
 }
