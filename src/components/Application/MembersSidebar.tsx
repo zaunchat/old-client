110e3427b0bbb2529c6d431e8abde011815fe7d8
z7pz <sdfygamer@gmail.com>
2022-07-24 16:27:03 +0300

    fix: map categories

--- src/components/Application/MembersSidebar.tsx
+++ src/components/Application/MembersSidebar.tsx
@@ -1,45 +1,65 @@
 import { h } from "preact";
+import { useState } from "preact/hooks";
 import styles from "../styles/Application/MembersSidebar.module.scss";
 
-function Member() {
+interface IMember {
+  username: string;
+  status: string;
+  avatar: string;
+}
+
+interface ICategory {
+  name: string;
+  members: IMember[];
+}
+
+function Member({ member }: { member: IMember }) {
   return (
     <div class={styles.member}>
-      <img
-        src="https://images-ext-2.discordapp.net/external/IHYqSv1JWvKfRoo6etajisbHqZXS2ortFa70RqNZT04/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/508449321176268801/4c4ac0cf1b75991c6a61059a72cf4c2c.png"
-        alt=""
-      />
+      <img src={member.avatar} alt="" />
       <div class={styles.user}>
-        <div class={styles.username}>Test</div>
+        <div class={styles.username}>{member.username}</div>
         <div class={styles.status}>
           <div class={styles.icon} />
-          <div class={styles.type}>Active now</div>
+          <div class={styles.type}>{member.status}</div>
         </div>
       </div>
     </div>
   );
 }
-function Members() {
+function Members({ members }: { members: IMember[] }) {
   return (
     <div class={styles.members}>
-      <Member />
-      <Member />
-      <Member />
+      {members.map((member) => (
+        <Member member={member} />
+      ))}
     </div>
   );
 }
-function Category() {
+function Category({ category }: { category: ICategory }) {
   return (
     <div class={styles.category}>
-      <div class={styles.title}>Members</div>
-      <Members />
+      <div class={styles.title}>{category.name}</div>
+      <Members members={category.members} />
     </div>
   );
 }
 export function MembersSidebar() {
+  const [categories, setCategories] = useState<ICategory[]>(
+    Array(2).fill({
+      name: "Members",
+      members: Array(10).fill({
+        username: "Mr.Kasper",
+        status: "Active now",
+        avatar: "https://images-ext-2.discordapp.net/external/IHYqSv1JWvKfRoo6etajisbHqZXS2ortFa70RqNZT04/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/508449321176268801/4c4ac0cf1b75991c6a61059a72cf4c2c.png",
+      }),
+    })
+  );
   return (
     <div class={styles.container}>
-      <Category />
-      <Category />
+      {categories.map((category) => (
+        <Category category={category} />
+      ))}
     </div>
   );
 }
