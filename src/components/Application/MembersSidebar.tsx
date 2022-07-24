4bd273a81d44b9e8ba574a7b77ca78cb7b86badd
z7pz <sdfygamer@gmail.com>
2022-07-24 16:18:03 +0300

    feat: add members sidebar

--- src/components/Application/MembersSidebar.tsx
+++ src/components/Application/MembersSidebar.tsx
@@ -1,9 +1,45 @@
-import { h } from 'preact';
+import { h } from "preact";
+import styles from "../styles/Application/MembersSidebar.module.scss";
 
-export function MembersSidebar() {
-    return (
-        <div>
-            test
+function Member() {
+  return (
+    <div class={styles.member}>
+      <img
+        src="https://images-ext-2.discordapp.net/external/IHYqSv1JWvKfRoo6etajisbHqZXS2ortFa70RqNZT04/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/508449321176268801/4c4ac0cf1b75991c6a61059a72cf4c2c.png"
+        alt=""
+      />
+      <div class={styles.user}>
+        <div class={styles.username}>Test</div>
+        <div class={styles.status}>
+          <div class={styles.icon} />
+          <div class={styles.type}>Active now</div>
         </div>
-    )
+      </div>
+    </div>
+  );
+}
+function Members() {
+  return (
+    <div class={styles.members}>
+      <Member />
+      <Member />
+      <Member />
+    </div>
+  );
+}
+function Category() {
+  return (
+    <div class={styles.category}>
+      <div class={styles.title}>Members</div>
+      <Members />
+    </div>
+  );
+}
+export function MembersSidebar() {
+  return (
+    <div class={styles.container}>
+      <Category />
+      <Category />
+    </div>
+  );
 }
---
+++ src/components/styles/Application/MembersSidebar.module.scss.d.ts
@@ -0,0 +1,24 @@
+declare const classNames: {
+  readonly container: "container";
+  readonly category: "category";
+  readonly title: "title";
+  readonly members: "members";
+  readonly member: "member";
+  readonly user: "user";
+  readonly username: "username";
+  readonly status: "status";
+  readonly icon: "icon";
+  readonly type: "type";
+};
+export default classNames;
+export type ClassNames =
+  | "container"
+  | "category"
+  | "title"
+  | "members"
+  | "member"
+  | "user"
+  | "username"
+  | "status"
+  | "icon"
+  | "type";
