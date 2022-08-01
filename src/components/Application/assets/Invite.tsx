1b402f586a00dda9f578abe9614d0aa82ddfa9b9
z7pz <sdfygamer@gmail.com>
2022-08-01 23:41:34 +0300

    feat: add invite button

---
+++ src/components/Application/assets/Invite.tsx
@@ -0,0 +1,40 @@
+import { h } from "preact";
+
+export function InvitePersonIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M14 2H16V3H14V5H13V3H11V2H13V0H14V2Z" fill="#BCBCBC" />
+      <path
+        d="M6.5 8.00668C7.88 8.00668 9 6.88668 9 5.50668C9 4.12668 7.88 3.00668 6.5 3.00668C5.12 3.00668 4 4.12668 4 5.50668C4 6.88668 5.12 8.00668 6.5 8.00668Z"
+        fill="#BCBCBC"
+      />
+      <path
+        d="M6.5 8.34C3.26 8.34 1 9.98666 1 12.34V13.0067H12V12.34C12 9.98 9.74 8.34 6.5 8.34Z"
+        fill="#BCBCBC"
+      />
+    </svg>
+  );
+}
+
+export function InviteAddIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 6.22223H7.77778V0H6.22222V6.22223H0V7.77779H6.22222V14H7.77778V7.77779H14V6.22223Z"
+        fill="#5D52FB"
+      />
+    </svg>
+  );
+}
