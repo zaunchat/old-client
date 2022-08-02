dc977e426e4706c879de42d0dc10c955dd6efc86
z7pz <sdfygamer@gmail.com>
2022-08-02 20:52:42 +0300

    feat: add welcomer to chat area

--- src/components/Application/Application.tsx
+++ src/components/Application/Application.tsx
@@ -4,12 +4,35 @@ import ReactTextareaAutosize from "react-textarea-autosize";
 
 import styles from "../styles/Application/Application.module.scss";
 import { EmojiIcon, SendIcon, ShareFilesIcon } from "./assets";
+import { DogWelcomerVector, PartyWelcomeVecotr, WelcomeBlob } from "./assets/Welcomer";
+
+
+
+function Welcomer() {
+  return (
+    <div class={styles.welcomer}>
+      <div class={styles.header}>
+        <div class={styles.party_vector}><PartyWelcomeVecotr /></div>
+        <div class={styles.welcome}>WELCOME TO</div>
+        <div class={styles.name}>SPOTIFY LISTS</div>
+        <div class={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor mollis leo proin turpis eu hac. Torto</div>
+      </div>
+      <div class={styles.dog_vector}><DogWelcomerVector /></div>
+      <div class={styles.welcome_blob}><WelcomeBlob /></div>
+    </div>
+  )
+}
+
 
 export function App() {
   const [inputRef, setInputRef] = useState<HTMLAreaElement>();
   return (
     <div class={styles.container}>
-      <div class={styles.area}></div>
+      <div class={styles.area}>
+        <div class={styles.messages}>
+        <Welcomer />
+        </div>
+      </div>
       <div class={styles.box}>
         <div class={styles.container}>
           <ReactTextareaAutosize
--- src/components/styles/Application/Application.module.scss.d.ts
+++ src/components/styles/Application/Application.module.scss.d.ts
@@ -1,6 +1,14 @@
 declare const classNames: {
   readonly container: "container";
   readonly area: "area";
+  readonly messages: "messages";
+  readonly welcomer: "welcomer";
+  readonly welcome_blob: "welcome_blob";
+  readonly dog_vector: "dog_vector";
+  readonly header: "header";
+  readonly welcome: "welcome";
+  readonly name: "name";
+  readonly description: "description";
   readonly box: "box";
   readonly textarea: "textarea";
   readonly icons: "icons";
@@ -10,6 +18,14 @@ export default classNames;
 export type ClassNames =
   | "container"
   | "area"
+  | "messages"
+  | "welcomer"
+  | "welcome_blob"
+  | "dog_vector"
+  | "header"
+  | "welcome"
+  | "name"
+  | "description"
   | "box"
   | "textarea"
   | "icons"
