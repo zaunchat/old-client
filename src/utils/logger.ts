284fc4daecc241823d687e85217c9dd4e529e401
z7pz <sdfygamer@gmail.com>
2022-08-03 20:08:56 +0300

    feat: add another logger utils

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,6 +2,17 @@ interface ILoggerOptions {
   state: "development" | "production";
 }
 
+interface IDefaultOptions {
+  title: string;
+  custom_color?: string;
+  with_title?: boolean;
+}
+
+interface IInfoOptions extends IDefaultOptions {}
+interface IErrorOptions extends IDefaultOptions {}
+interface IDebugOptions extends IDefaultOptions {}
+interface IWarnOptions extends IDefaultOptions {}
+
 class Logger {
   state: ILoggerOptions["state"];
   constructor(options: ILoggerOptions) {
@@ -10,16 +21,16 @@ class Logger {
   log(...args: any[]) {
     console.log(...args);
   }
-  info(...args: any[]) {
-    console.log(...args);
+  info(message: string, options?: IInfoOptions) {
+    console.log(`${options?.title ? `[${options.title}]` : `[INFO]`}: ${message}`);
   }
-  debug(...args: any[]) {
-    console.log();
+  debug(message: string, options?: IDebugOptions) {
+    console.log(`${options?.title ? `[${options.with_title ? "DEBUG:" : ""}${options.title}]` : `[DEBUG]`}: ${message}`);
   }
-  error() {
-    console.log();
+  error(message: string, options?: IErrorOptions) {
+    console.log(`${options?.title ? `[${options.with_title ? "ERROR:" : ""}${options.title}]` : `[ERROR]`}: ${message}`);
   }
-  warn() {
-    console.log();
+  warn(message: string, options?: IWarnOptions) {
+    console.log(`${options?.title ? `[${options.with_title ? "WARN:" : ""}${options.title}]` : `[WARN]`}: ${message}`);
   }
 }
