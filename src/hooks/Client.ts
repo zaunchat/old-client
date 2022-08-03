51d285625f8855d506307989fe4650de9228900f
z7pz <sdfygamer@gmail.com>
2022-08-03 18:12:27 +0300

    fix: add development mode

--- src/hooks/Client.ts
+++ src/hooks/Client.ts
@@ -1,19 +1,21 @@
-import { Client } from '@itchatapp/client'
-import { createContext } from 'preact';
-import { useContext } from 'preact/hooks';
+import { Client } from "@itchatapp/client";
+import { createContext } from "preact";
+import { useContext } from "preact/hooks";
 
 // FOR DEVELOPMENT PURPOSES ONLY
 const client = new Client(
-    {
+  process.env.NODE_ENV === "development"
+    ? {
         rest: { api: "https://api-dev.itchat.world" },
         ws: {
           url: "wss://api-dev.itchat.world/ws",
         },
       }
+    : {}
 );
 
-client.on('ready', () => {
-    console.log(`${client.user?.username} is ready!`);
+client.on("ready", () => {
+  console.log(`${client.user?.username} is ready!`);
 });
 
 export const ClientContext = createContext(client);
