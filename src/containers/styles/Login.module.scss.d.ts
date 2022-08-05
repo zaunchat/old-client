5c976d04bdc115cc45887182d7e5947b6d708f80
Zsl8 <zezoalajwadi@gmail.com>
2022-08-05 21:23:10 +0300

    feat: add login page with responsive

--- src/containers/styles/Login.module.scss.d.ts
+++ src/containers/styles/Login.module.scss.d.ts
@@ -1,5 +1,26 @@
 declare const classNames: {
   readonly container: "container";
+  readonly login: "login";
+  readonly login_logo: "login_logo";
+  readonly login_title: "login_title";
+  readonly login_inputs: "login_inputs";
+  readonly login_input: "login_input";
+  readonly line: "line";
+  readonly rememberMe: "rememberMe";
+  readonly login_buttons: "login_buttons";
+  readonly login_button: "login_button";
+  readonly vector: "vector";
 };
 export default classNames;
-export type ClassNames = "container";
+export type ClassNames =
+  | "container"
+  | "login"
+  | "login_logo"
+  | "login_title"
+  | "login_inputs"
+  | "login_input"
+  | "line"
+  | "rememberMe"
+  | "login_buttons"
+  | "login_button"
+  | "vector";
