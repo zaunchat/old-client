6a6a2c48f0f23659a2f455c4a46d1021876666a7
z7pz <sdfygamer@gmail.com>
2022-11-02 19:39:21 +0300

    feat: add search icon to search input

--- src/components/Application/Sidebar/Servers.tsx
+++ src/components/Application/Sidebar/Servers.tsx
@@ -1,5 +1,5 @@
 import { h } from 'preact';
-import { useState } from 'preact/hooks';
+import { useRef, useState } from 'preact/hooks';
 import styles from '../styles/Sidebar/Servers.module.scss';
 
 function Server() {
@@ -31,6 +31,7 @@ function Server() {
 }
 
 export function ServersSidebar() {
+  const searchInputRef = useRef(null);
   const [collapse, setCollapse] = useState(false);
   return (
     <div className={`${styles.container} ${collapse ? styles.collapsed : ``}`}>
@@ -58,8 +59,34 @@ export function ServersSidebar() {
           <div className={styles.divider}></div>
         </div>
         <div className={styles.input_wrapper}>
-          <input placeholder="SERVERS SEARCH..." type="text" />
-          <span></span>
+          <input
+            ref={searchInputRef}
+            placeholder="SERVERS SEARCH..."
+            type="text"
+          />
+          <div
+            onClick={() => {
+              if (collapse) {
+                setCollapse(false);
+                searchInputRef.current?.focus();
+              } else {
+                searchInputRef.current?.focus();
+              }
+            }}
+          >
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 16 16"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M14.5006 13.3763L10.6231 9.49816C11.2479 8.63945 11.5841 7.60454 11.5831 6.54253C11.5831 3.76285 9.32149 1.50128 6.5418 1.50128C3.76211 1.50128 1.50055 3.76285 1.50055 6.54253C1.50055 9.32222 3.76211 11.5838 6.5418 11.5838C7.60381 11.5848 8.63872 11.2487 9.49743 10.6238L13.3756 14.5013L14.5006 13.3763ZM6.5418 9.9916C5.85955 9.99166 5.19261 9.7894 4.62531 9.4104C4.05802 9.0314 3.61585 8.49269 3.35474 7.86238C3.09363 7.23208 3.02529 6.5385 3.15838 5.86936C3.29146 5.20021 3.61999 4.58557 4.10241 4.10314C4.58483 3.62072 5.19948 3.29219 5.86862 3.15911C6.53777 3.02602 7.23135 3.09436 7.86165 3.35547C8.49196 3.61659 9.03067 4.05875 9.40967 4.62604C9.78867 5.19334 9.99093 5.86028 9.99086 6.54253C9.98979 7.45695 9.62606 8.33361 8.97947 8.9802C8.33288 9.62679 7.45622 9.99052 6.5418 9.9916Z"
+                fill="#777777"
+              />
+            </svg>
+          </div>
         </div>
         <div className={styles.servers}>
           <Server />
